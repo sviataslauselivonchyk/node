62a1bc9d17c7fa9ec5fd2c5245a8c8d8d390458e
Slava Selivonchyk <hiblton91@gmail.com>
2018-12-06 23:40:02 +0300

    Second commit

--- task1.js
+++ task1.js
@@ -3,18 +3,14 @@ var git = require("git-rev-sync");
 
 console.log(git.short());
 console.log(git.branch());
-console.log(git.tag());  
+console.log(git.tag());
 
-let versionFileContent = "export const version = 'v1.0.0'";
+let versionFileContent = "export const version = '" + git.tag() + "'"; //  todo template syntax
 
-if (fs.existsSync("version.ts")) {
-    versionFileContent = fs.readFileSync("version.ts", "utf8");
-    versionFileContent += ' !!! ';
-}
+// if (fs.existsSync("version.ts")) {
+//     versionFileContent = fs.readFileSync("version.ts", "utf8");
+// }
 
-fs.writeFile("version.ts", versionFileContent, function (error) {
-    if (error) throw error; // если возникла ошибка
-    console.log("Асинхронная запись файла завершена. Содержимое файла:");
-    let data = fs.readFileSync("version.ts", "utf8");
-    console.log(data); // выводим считанные данные
-});
+fs.writeFileSync("version.ts", versionFileContent);
+versionFileContent = fs.readFileSync("version.ts", "utf8");
+console.log(versionFileContent);
